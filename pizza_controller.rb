d7585d16d985bf7df0360ab1d812f719203437c3
Joao <johnynecas@hotmail.com>
2017-10-17 15:44:53 +0100

    shop done

---
+++ pizza_controller.rb
@@ -0,0 +1,41 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+# require( 'pry-byebug' )
+require_relative('./models/pizza.rb')
+
+get'/pizzas' do
+  @pizzas = Pizza.all()
+  erb(:index)
+end
+ # got to put the new page before the id pages to that sinatra doesnt get confused
+get '/pizzas/new' do
+erb(:new)
+end
+
+post '/pizzas' do
+@pizza = Pizza.new(params)
+ @pizza.save()
+ erb(:create)
+end
+
+get '/pizzas/:id' do
+@pizza = Pizza.find(params[:id].to_i)
+erb(:show)
+end
+
+post '/pizzas/:id/delete' do
+@pizza = Pizza.find(params[:id]).delete
+# erb(:delete)
+redirect "/pizzas"
+end
+
+get '/pizzas/:id/edit' do
+@pizza = Pizza.find(params[:id])
+erb(:edit)
+end
+
+post '/pizzas/:id' do
+@pizza = Pizza.new(params)
+@pizza.update()
+redirect "/pizzas"
+end
